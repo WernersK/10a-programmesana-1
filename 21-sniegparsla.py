9507eb7a1754c573b100507a6da1065978eaf23e
emils23 <71400330+emils23@users.noreply.github.com>
2020-12-16 11:21:30 +0200

    klases darbs

---
+++ 21-sniegparsla.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+
+
+
+import turtle
+import random
+elsa = turtle.Turtle()
+turtle.Screen().bgcolor("purple")
+colours = ["cyan","grey","white","blue"]
+elsa.color("cyan")
+for i in range(10):
+    for i in range(2):
+        elsa.forward(100)
+        elsa.right(60)
+        elsa.forward(100)
+        elsa.right(120)
+    elsa.right(36)
